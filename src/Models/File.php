aba7aaf2d1281c8d62b075a890fe2731df8c6e51
Mateusz Grochala <mateusz.grochala@yahoo.dk>
2019-10-08 15:24:24 +0200

    removed dependency

--- src/Models/File.php
+++ src/Models/File.php
@@ -3,10 +3,10 @@
 namespace MorningTrain\Laravel\Fields\Files\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Storage;
-use MorningTrain\Laravel\Fields\Files\Support\Filepond;
 use Illuminate\Http\File as FileHTTP;
-use Ramsey\Uuid\Uuid;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use MorningTrain\Laravel\Fields\Files\Support\Filepond;
 
 class File extends Model
 {
@@ -28,7 +28,7 @@ class File extends Model
 
         $this->size = $file->getSize();
         $this->mime = $file->getMimeType();
-        $this->uuid = Uuid::uuid4()->toString();
+        $this->uuid = (string) Str::uuid();
         $this->disk = 'local'; //TODO: Make disk dynamic
         $this->location = 'filepond'; // TODO: Make location dynamic
 
