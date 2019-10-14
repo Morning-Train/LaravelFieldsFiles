11d0d4db96f4589f6171ff5939d7e0d23de3d1c8
Mateusz Grochala <mateusz.grochala@yahoo.dk>
2019-10-14 16:14:41 +0200

    Load functionality

--- src/Operations/Filepond/Load.php
+++ src/Operations/Filepond/Load.php
@@ -2,10 +2,9 @@
 
 namespace MorningTrain\Laravel\Fields\Files\Operations\Filepond;
 
-use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 use MorningTrain\Laravel\Fields\Files\Filepond;
 use MorningTrain\Laravel\Resources\Support\Contracts\Operation;
-use Illuminate\Support\Facades\Response;
 
 class Load extends Operation
 {
@@ -14,15 +13,10 @@ class Load extends Operation
 
     public function handle($model = null)
     {
-        $path = storage_path('app/' . Filepond::getPathFromServerId(request()->route('filepond')));
+        $info = Filepond::getInfoFromServerId(request()->route('filepond'));
+        $name = "{$info->name}.{$info->extension}";
 
-        $file = File::get($path);
-        $type = File::mimeType($path);
-
-        $response = Response::make($file, 200);
-        $response->header('Content-Type', $type);
-
-        return $response;
+        return Storage::disk($info->disk)->response($info->path, $name);
     }
 
 }
